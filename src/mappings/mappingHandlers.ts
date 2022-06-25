d4bbdf9f2837c5ff1c70c446a404f655b8b136db
royki <kearan.rayofsun@gmail.com>
2022-06-25 07:40:12 -0400

    subquery 1st commit

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,23 @@
+import {SubstrateEvent} from "@subql/types";
+import { Transfer } from "../types";
+import { Balance } from "@polkadot/types/interfaces";
+
+export async function handleEvent(event: SubstrateEvent): Promise<void> {
+  // Get data from the event
+  // The balances.transfer event has the following payload \[from, to, value\]
+  // logger.info(JSON.stringify(event));
+  const from = event.event.data[0];
+  const to = event.event.data[1];
+  const amount = event.event.data[2];
+
+  // Create the new transfer entity
+  const transfer = new Transfer(
+    `${event.block.block.header.number.toNumber()}-${event.idx}`
+  );
+  transfer.blockNumber = event.block.block.header.number.toBigInt();
+  transfer.from = from.toString();
+  transfer.to = to.toString();
+  transfer.amount = (amount as Balance).toBigInt();
+  await transfer.save();
+}
+
